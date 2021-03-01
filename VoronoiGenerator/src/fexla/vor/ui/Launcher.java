9666a53fdc1e1630abc15edbcd0d532f78b9c617
fexla <41383116+fexla@users.noreply.github.com>
2021-03-01 15:13:19 +0800

    build project

---
+++ VoronoiGenerator/src/fexla/vor/ui/Launcher.java
@@ -0,0 +1,12 @@
+package fexla.vor.ui;
+
+/**
+ * @author ：fexla
+ * @description：TODO
+ * @date ：2021/3/1 10:46
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
